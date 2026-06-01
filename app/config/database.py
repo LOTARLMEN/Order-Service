c4d39561eb1ed5f0a5fec836d050b838ec05e2ea
lotarlmen <val.janushkewitch@yandex.ru>
2026-06-01 17:32:52 +0200

    database config

---
+++ app/config/database.py
@@ -0,0 +1,20 @@
+from pydantic_settings import SettingsConfigDict
+
+from app.config.base_setting import BaseConfig
+
+
+class DatabaseConfig(BaseConfig):
+    model_config = SettingsConfigDict(env_prefix="POSTGRES_")
+
+    HOST: str
+    PORT: int
+    USERNAME: str
+    PASSWORD: str
+    DATABASE_NAME: str
+
+    @property
+    def DATABASE_URL(self):
+        return (
+            f"postgresql+asyncpg://{self.USERNAME}:{self.PASSWORD}@"
+            f"{self.HOST}:{self.PORT}/{self.DATABASE_NAME}"
+        )
