1d42418129049a4da1f78f2131d5470b2762601b
lotarlmen <val.janushkewitch@yandex.ru>
2026-06-01 17:34:59 +0200

    create Order Repo

---
+++ app/infrastructure/repositories/order/order.py
@@ -0,0 +1,73 @@
+from uuid import UUID
+
+from sqlalchemy import select
+from sqlalchemy.dialects.postgresql import insert
+
+from app.core.models import (
+    Item,
+    Order,
+    OrderStatusEnum,
+    OrderStatusHistory,
+)
+from app.infrastructure.db.database_schemas.order import Order as DBOrder
+from app.infrastructure.db.database_schemas.order import OrderStatus
+from app.infrastructure.repositories.base import BaseRepository
+from app.infrastructure.repositories.exeptions import DoesNotExist
+from app.infrastructure.repositories.order.order_dto import OrderDTO
+
+
+class OrderRepository(BaseRepository):
+    @staticmethod
+    def _construct(db_order: DBOrder) -> Order:
+        if not db_order:
+            raise DoesNotExist
+
+        return Order(
+            id=db_order.id,
+            user_id=db_order.user_id,
+            items=[Item(**item) for item in db_order.items],
+            amount=db_order.amount,
+            status=OrderStatusEnum(db_order.statuses[0].status),
+            status_history=[
+                OrderStatusHistory(
+                    status=OrderStatusEnum(s.status), created_at=s.created_at
+                )
+                for s in db_order.statuses
+            ],
+        )
+
+    async def create(self, order: OrderDTO) -> Order:
+        stmt_order = (
+            insert(DBOrder)
+            .values(
+                {
+                    "user_id": order.user_id,
+                    "items": order.items,
+                    "amount": order.amount,
+                }
+            )
+            .returning(DBOrder)
+        )
+
+        order_result = (await self._session.execute(stmt_order)).scalar()
+
+        stmt_status = insert(OrderStatus).values(
+            {
+                "user_id": order_result.id,
+                "status": order.status,
+            }
+        )
+
+        await self._session.execute(stmt_status)
+
+        order = await self.get_by_id(order_result.id)
+        return self._construct(order)
+
+    async def get_by_id(self, order_id: UUID) -> DBOrder:
+        stmt = select(DBOrder).where(DBOrder.id == order_id)
+        order = (await self._session.execute(stmt)).scalar_one_or_none()
+
+        if order is None:
+            raise ValueError(f"Order with id {order_id} not found")
+
+        return order
