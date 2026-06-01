bc103416845f75bc38847054aa266cab96f071c1
lotarlmen <val.janushkewitch@yandex.ru>
2026-06-01 17:32:07 +0200

    crate BaseConfig, that simple to use

---
+++ app/config/base_setting.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ROOT_DIR = Path(__file__).resolve().parents[2]
+ENV_FILE = ROOT_DIR / ".env"
+
+
+class BaseConfig(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE,
+        env_file_encoding="utf-8",
+        env_ignore_empty=True,
+        extra="ignore",
+    )
