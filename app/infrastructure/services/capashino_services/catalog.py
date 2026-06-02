2a3b3bc3374d1290233d61bc6812e66e76b01df3
lotarlmen <val.janushkewitch@yandex.ru>
2026-06-02 19:11:22 +0200

    fix tabulations

--- app/infrastructure/services/capashino_services/catalog.py
+++ app/infrastructure/services/capashino_services/catalog.py
@@ -1,17 +1,19 @@
 from urllib.parse import urljoin
 from uuid import UUID
 
-from ...core.models import Item
+from app.core.models import Item
+
 from .base import CapashinoBaseHTTPClient
 
 
 class CatalogServiceClient(CapashinoBaseHTTPClient):
     def __init__(self) -> None:
         super().__init__(
-            client_name="Catalog Service", client_url="/api/catalog/items/"
+            client_name="Catalog Service",
+            client_url="/api/catalog/items/",
         )
 
     async def get_item(self, item_id: UUID) -> Item:
         path = urljoin(self._client_url, str(item_id))
-        item = await self._send_request("GET", path=path)
+        item = await self._send_request(method="GET", path=path)
         return Item(**item)
