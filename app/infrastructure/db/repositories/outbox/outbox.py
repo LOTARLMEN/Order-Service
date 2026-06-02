ea81278716603ed0aca0d7d02fd793f6f26bc269
lotarlmen <val.janushkewitch@yandex.ru>
2026-06-02 19:09:23 +0200

    fix imports and add with_for_update(skip_locked=True) for get_pending_events

--- app/infrastructure/db/repositories/outbox/outbox.py
+++ app/infrastructure/db/repositories/outbox/outbox.py
@@ -4,14 +4,14 @@ from sqlalchemy import select, update
 from sqlalchemy.dialects.postgresql import insert
 
 from app.core.models import (
-    EventTypeEnum,
     OutboxEvent,
     OutboxEventStatus,
+    OutboxEventTypeEnum,
 )
 from app.infrastructure.db.database_schemas.outbox import Outbox as DBOutbox
 from app.infrastructure.db.repositories.base import BaseRepository
 from app.infrastructure.db.repositories.exeptions import DoesNotExist
-from app.infrastructure.db.repositories.outbox.outbox_dto import OutboxDTO
+from app.infrastructure.db.repositories.outbox.outbox_dto import OutboxEventDTO
 
 
 class OutboxRepository(BaseRepository):
@@ -19,13 +19,13 @@ class OutboxRepository(BaseRepository):
     def _construct(db_outbox: DBOutbox) -> OutboxEvent:
         return OutboxEvent(
             id=db_outbox.id,
-            event_type=EventTypeEnum(db_outbox.event_type),
+            event_type=OutboxEventTypeEnum(db_outbox.event_type),
             payload=db_outbox.payload,
             status=OutboxEventStatus(db_outbox.status),
             created_at=db_outbox.created_at,
         )
 
-    async def create(self, event: OutboxDTO) -> OutboxEvent:
+    async def create(self, event: OutboxEventDTO) -> OutboxEvent:
         stmt = (
             insert(OutboxEvent)
             .values(
@@ -38,17 +38,18 @@ class OutboxRepository(BaseRepository):
             .returning(OutboxEvent)
         )
 
-        outbox = (await self._session.execute(stmt)).scalar_one_or_none()
-        if not outbox:
-            raise RuntimeError("Failed to create outbox event: database returned None")
+        outbox_event = (await self._session.execute(stmt)).scalar_one_or_none()
+        if not outbox_event:
+            raise RuntimeError("Failed to create outbox event: database returned None.")
 
-        return self._construct(outbox)
+        return self._construct(outbox_event)
 
     async def get_pending_events(self, limit: int = 100) -> list[OutboxEvent]:
         stmt = (
             select(DBOutbox)
             .where(DBOutbox.event_type == OutboxEventStatus.PENDING)
             .limit(limit)
+            .with_for_update(skip_locked=True)
         )
         events = (await self._session.execute(stmt)).scalars()
         return [self._construct(event) for event in events]
@@ -60,7 +61,7 @@ class OutboxRepository(BaseRepository):
             raise DoesNotExist
         return self._construct(event)
 
-    async def update(self, event_id: UUID) -> OutboxEvent:
+    async def update(self, event_id: UUID):
         stmt = (
             update(DBOutbox)
             .where(DBOutbox.id == event_id)
