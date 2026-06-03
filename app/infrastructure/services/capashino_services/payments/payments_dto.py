2d567f4c8da2041bb4762b676ae72bad143efe63
lotarlmen <val.janushkewitch@yandex.ru>
2026-06-03 15:19:48 +0200

    fix dto model

--- app/infrastructure/services/capashino_services/payments/payments_dto.py
+++ app/infrastructure/services/capashino_services/payments/payments_dto.py
@@ -3,9 +3,11 @@ from uuid import UUID
 
 from pydantic import BaseModel
 
+from app.config.config import settings
+
 
 class PaymentDTO(BaseModel):
     order_id: UUID
     amount: Decimal
-    callback_url: str
+    callback_url: str = settings.OrderService.SHIPPING_URL
     idempotency_key: str | UUID
