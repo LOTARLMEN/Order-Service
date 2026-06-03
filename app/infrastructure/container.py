dc3e87006a93ca7891941f1de5d3a280c0826b95
lotarlmen <val.janushkewitch@yandex.ru>
2026-06-03 15:18:56 +0200

    finished infrustructure container

--- app/infrastructure/container.py
+++ app/infrastructure/container.py
@@ -4,27 +4,23 @@ from dependency_injector import containers, providers
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-# from app.infrastructure.kafka_producer import KafkaProducer
-# from app.infrastructure.unit_of_work import UnitOfWork
+from app.infrastructure.services.kafka.producer.producer import KafkaProducerService
+from app.infrastructure.unit_of_work import UnitOfWork
 
 
 class InfrastructureContainer(containers.DeclarativeContainer):
-    config = providers.Configuration()
+    setting = providers.Configuration()
     async_engine = providers.Singleton[AsyncEngine](
         create_async_engine,
-        config.db.dsn,
-        pool_size=config.db.pool_size,
-        pool_recycle=config.db.pool_recycle,
-        future=True,
+        setting.Database.URL,
     )
     session_factory: Callable[..., AsyncSession] = providers.Factory(
         sessionmaker, async_engine, expire_on_commit=False, class_=AsyncSession
     )
-    # unit_of_work = providers.Singleton[UnitOfWork](
-    #     UnitOfWork, session_factory=session_factory
-    # )
-    # kafka_producer = providers.Singleton[KafkaProducer](
-    #     KafkaProducer,
-    #     bootstrap_servers=config.kafka.bootstrap_servers,
-    #     topic=config.kafka.topic,
-    # )
+    unit_of_work = providers.Singleton[UnitOfWork](
+        UnitOfWork, session_factory=session_factory
+    )
+    kafka_producer = providers.Singleton[KafkaProducerService](
+        KafkaProducerService,
+        url=setting.Kafka.BOOTSTRAP_SERVERS,
+    )
