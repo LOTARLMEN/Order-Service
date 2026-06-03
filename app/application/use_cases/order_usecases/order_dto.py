59714935b1849640573876076d15ce4e24b9e645
lotarlmen <val.janushkewitch@yandex.ru>
2026-06-03 18:58:35 +0200

    fix model

--- app/application/use_cases/order_usecases/order_dto.py
+++ app/application/use_cases/order_usecases/order_dto.py
@@ -1,5 +1,3 @@
-from decimal import Decimal
-
 from pydantic import BaseModel
 
 from app.core.models import Item, OrderStatusEnum
@@ -7,6 +5,5 @@ from app.core.models import Item, OrderStatusEnum
 
 class OrderDTO(BaseModel):
     user_id: str
-    items: list[Item]
-    amount: Decimal
+    item: Item
     status: OrderStatusEnum
