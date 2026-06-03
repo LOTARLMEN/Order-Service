c583bb7fdd074099f464cdaaa258f2b9ed5c371d
lotarlmen <val.janushkewitch@yandex.ru>
2026-06-03 18:55:22 +0200

    change method add to db

--- app/infrastructure/db/repositories/outbox/outbox.py
+++ app/infrastructure/db/repositories/outbox/outbox.py
@@ -1,50 +1,41 @@
 from uuid import UUID
 
 from sqlalchemy import select, update
-from sqlalchemy.dialects.postgresql import insert
 
 from app.application.use_cases.outbox_usecases.outbox_dto import OutboxEventDTO
 from app.core.models import (
+    OrderEventType,
     OutboxEvent,
     OutboxEventStatus,
-    OutboxEventTypeEnum,
 )
 from app.infrastructure.db.database_schemas.outbox import Outbox as DBOutbox
 from app.infrastructure.db.repositories.base import BaseRepository
-from app.infrastructure.db.repositories.exeptions import DoesNotExist
 
 
 class OutboxRepository(BaseRepository):
     @staticmethod
-    def _construct(db_outbox: DBOutbox) -> OutboxEvent:
+    def _construct(db_outbox: DBOutbox | None) -> OutboxEvent | None:
+        if not db_outbox:
+            return None
         return OutboxEvent(
             id=db_outbox.id,
-            event_type=OutboxEventTypeEnum(db_outbox.event_type),
+            event_type=OrderEventType(db_outbox.event_type),
             payload=db_outbox.payload,
             status=OutboxEventStatus(db_outbox.status),
             created_at=db_outbox.created_at,
         )
 
-    async def create(self, event: OutboxEventDTO) -> OutboxEvent:
-        stmt = (
-            insert(OutboxEvent)
-            .values(
-                {
-                    "event_type": event.event_type,
-                    "payload": event.payload,
-                    "status": OutboxEventStatus.PENDING,
-                }
-            )
-            .returning(OutboxEvent)
+    async def create(self, event: OutboxEventDTO) -> OutboxEvent | None:
+        db_outbox_event = DBOutbox(
+            event_type=event.event_type,
+            payload=event.payload,
+            status=event.status,
         )
+        self._session.add(db_outbox_event)
+        await self._session.flush()
+        return self._construct(db_outbox_event)
 
-        outbox_event = (await self._session.execute(stmt)).scalar_one_or_none()
-        if not outbox_event:
-            raise RuntimeError("Failed to create outbox event: database returned None.")
-
-        return self._construct(outbox_event)
-
-    async def get_pending_events(self, limit: int = 100) -> list[OutboxEvent]:
+    async def get_pending_events(self, limit: int = 100) -> list[OutboxEvent | None]:
         stmt = (
             select(DBOutbox)
             .where(DBOutbox.event_type == OutboxEventStatus.PENDING)
@@ -54,11 +45,9 @@ class OutboxRepository(BaseRepository):
         events = (await self._session.execute(stmt)).scalars()
         return [self._construct(event) for event in events]
 
-    async def get_by_id(self, event_id: UUID) -> OutboxEvent:
-        stmt = select(OutboxEvent).where(DBOutbox.id == event_id)
+    async def get_by_id(self, event_id: UUID) -> OutboxEvent | None:
+        stmt = select(DBOutbox).where(DBOutbox.id == event_id)
         event = (await self._session.execute(stmt)).scalar_one_or_none()
-        if not event:
-            raise DoesNotExist
         return self._construct(event)
 
     async def update(self, event_id: UUID):
